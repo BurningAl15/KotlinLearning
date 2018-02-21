5125561431b36e9d22c6eac45b5666894f6246b3
BurningAl15 <al_1552009@hotmail.com>
2018-02-21 02:07:47 -0500

    When is like switch but with swag

--- untitled/src/FindMyAge.kt
+++ untitled/src/FindMyAge.kt
@@ -31,20 +31,35 @@ fun main(args:Array<String>)
     //Output
     print(age)
 
-
+    print("\n")
     //When
     //This decision structure is like the switch in c/c++
     var x= readLine()!!.toInt()
-
+    print("\n")
     when(x)
     {
-
-        1->print(" Value is 1")
-        2->print(" Value is 2")
+        1,2->print(" Value is 1 or 2")
         3->print(" Value is 3")
-        4->print(" Value is 4")
+        in 10..30->print(" Value is in range between 10 and 30")
+        !in 10..40->print(" Value is not in range between 10 and 40")
         else -> print(" Value is out of range")
     }
 
+    //Extra
+    //if
+    var n=20
+    var n2=30
+    var max=if(n>n2) n else n2
+    print("\n")
+    print("max:$max")
 
+    //when
+    var Age=30
+    var isYoung=when(Age)
+    {
+        30->true
+        else->false
+    }
+    print("\n")
+    print("isYoung:$isYoung")
 }
