4a0461cfcf713344bdc0d317ad0438bbf4b48262
BurningAl15 <al_1552009@hotmail.com>
2018-02-20 22:32:36 -0500

    FirstApp

---
+++ untitled/src/FirstApp.kt
@@ -0,0 +1,24 @@
+
+
+fun main(args:Array<String>)
+{
+    //print N println are almost the same if you want an output
+    println("Welcome 2 Kotlin!")
+
+    var name:String = "Aldhair"
+    var age:Int=28
+
+    var person:String?
+    person="Little pio"
+
+    print(name + " - "+ age + " " + person)
+
+
+    //If a value will not be updated later you must use val instead of var
+    val xName="Me"
+    //This line of code give an error, a val cannot be reasigned
+    //xName="Also me"
+
+    //
+
+}
